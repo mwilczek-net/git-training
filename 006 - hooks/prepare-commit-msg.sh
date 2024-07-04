76511ebee97ec4854a9037038856416bc70dd5aa
mwilczek-net <mwilczek-net@users.noreply.github.com>
2024-07-04 22:35:14 +0200

    prepare-commit-msg.sh

---
+++ 006 - hooks/prepare-commit-msg.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This hook adds Ticket ID as as prefix to commit message
+# Hook takes last part of branch name as Ticket ID
+# branch: feature/ALA-123
+# result: [ALA-123]
+# branch: hotfix/annoying/bug/BUG-432
+# result: [BUG-432]
+
+# Apply this hook only for given branches
+# Env variable overrides default setting
+if [ -z "${PREPARE_COMMIT_MSG_TICKET_ID_BRANCHES}" ]; then
+    PREPARE_COMMIT_MSG_TICKET_ID_BRANCHES=(feature hotfix bugfix)
+fi
+
+current_commit_message="${1}"
+
+current_branch_name=$(git symbolic-ref --short HEAD)
+current_branch_prefix="${current_branch_name%%/*}"
+ticket_id="${current_branch_name##*/}"
+
+current_branch_prefix_on_list=$(printf "%s\n" "${PREPARE_COMMIT_MSG_TICKET_ID_BRANCHES[@]}" | grep -c "^$current_branch_prefix$")
+ticket_id_in_current_commit_message=$(echo "${current_commit_message}" | grep -c "\[$ticket_id\]")
+
+
+if [ -n "$ticket_id" ] && [[ $current_branch_prefix_on_list -ne 0 ]] && [[ $ticket_id_in_current_commit_message -eq 0 ]]; then
+  sed -i.bak -e "1s/^/[$ticket_id] /" "${current_commit_message}"
+fi
